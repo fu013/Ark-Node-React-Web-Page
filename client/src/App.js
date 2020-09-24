9864dc9c47bf8f6c869a9a971689ed157653ab1d
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-24 17:50:47 +0900

    json파일 스테이트에 프로미스로 할당

--- client/src/App.js
+++ client/src/App.js
@@ -19,55 +19,32 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-// INSERT VALUE
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/128/128/any',
-  'name': '이승찬',
-  'birthday': '960208',
-  'gender': '남자',
-  'job': '취준생'
-  },
-  {
-  'id': 2,
-  'image': 'https://placeimg.com/128/129/any',
-  'name': '신영진',
-  'birthday': '961212',
-  'gender': '남자',
-  'job': '회사원'
-  }
-]
-
-// Customer
 // 태그를 단독으로 쓰는게 아니라면 무조건 상위 태그를 둘러줘야 한다.
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
-/*    하드코딩 ver - 1
-      <div>
-        <Customer 
-          id={customers[0].id}
-          image={customers[0].image}
-          name={customers[0].name}
-          birthday={customers[0].birthday}
-          gender={customers[0].gender}
-          job={customers[0].job}
-        />
-        <Customer 
-          id={customers[1].id}
-          image={customers[1].image}
-          name={customers[1].name}
-          birthday={customers[1].birthday}
-          gender={customers[1].gender}
-          job={customers[1].job}
-        />
-      </div> */
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
+      <Paper className={ classes.root }>
+        <Table className={ classes.table }>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -79,12 +56,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-              // customers를 c라는 이름으로 mapping함.
-              customers.map(c => {
-                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
-              })
-            }
+            {this.state.customers.map ? this.state.customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+              }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const port = process.env.PORT || 9983;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true})); 
 
- app.get('/api/hello', (req, res) => {
-     res.send({ meesage: 'Hello Scarlet'});
- });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/128/128/any',
+            'name': '이승찬',
+            'birthday': '960208',
+            'gender': '남자',
+            'job': '취준생'
+            },
+            {
+            'id': 2,
+            'image': 'https://placeimg.com/128/129/any',
+            'name': '신영진',
+            'birthday': '961212',
+            'gender': '남자',
+            'job': '회사원'
+            }
+    ]);
+})
 
  app.listen(port, () => console.log(`Listening on port ${port}`));
