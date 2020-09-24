9453d4d08093b337956eaf147446fd42f8aa0d97
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-24 10:15:37 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+// 카멜 형식 표기, classes의 css
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 // INSERT VALUE
 const customers = [
@@ -31,37 +45,52 @@ const customers = [
 // 태그를 단독으로 쓰는게 아니라면 무조건 상위 태그를 둘러줘야 한다.
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      // 하드코딩 ver - 1
-      // <div>
-      //   <Customer 
-      //     id={customers[0].id}
-      //     image={customers[0].image}
-      //     name={customers[0].name}
-      //     birthday={customers[0].birthday}
-      //     gender={customers[0].gender}
-      //     job={customers[0].job}
-      //   />
-      //   <Customer 
-      //     id={customers[1].id}
-      //     image={customers[1].image}
-      //     name={customers[1].name}
-      //     birthday={customers[1].birthday}
-      //     gender={customers[1].gender}
-      //     job={customers[1].job}
-      //   />
-      // </div>
+/*    하드코딩 ver - 1
       <div>
-        {
-          // customers를 c라는 이름으로 mapping함.
-          customers.map(c => {
-            return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
-          })
-        }
-      </div>
+        <Customer 
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        <Customer 
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+      </div> */
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              // customers를 c라는 이름으로 mapping함.
+              customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
 // Public Option
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -5,38 +5,41 @@ import TableCell from '@material-ui/core/TableCell';
 class Customer extends React.Component {
     render() {
         return (
-            <TableRow>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </TableRow>
+                <TableRow>
+                    <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                    <TableCell><h2>{this.props.name}</h2></TableCell>
+                    <TableCell><p>{this.props.id}</p></TableCell>
+                    <TableCell><p>{this.props.birthday}</p></TableCell>
+                    <TableCell><p>{this.props.gender}</p></TableCell>
+                    <TableCell><p>{this.props.job}</p></TableCell>
+                </TableRow>
         );
     }
 }
 
-// CUSTOMER PROFILE를 따로 구성하여, 이름 / 이미지를 한 그룹으로 구분
+/* // CUSTOMER PROFILE를 따로 구성하여, 이름 / 이미지를 한 그룹으로 구분
 class CustomerProfile extends React.Component {
     render() {
         return (
-            <TableRow>
-                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
-                <TableCell> <h2>{this.props.name}</h2><p>{this.props.id}</p></TableCell>
-            </TableRow>
+                <TableRow>
+                    <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                    <TableCell> <h2>{this.props.name}</h2><p>{this.props.id}</p></TableCell>
+                </TableRow>
         );
     }
 }
-
 // CUSTOMER INFO를 따로 구성하여, 이름 / 이미지를 한 그룹으로 구분
 class CustomerInfo extends React.Component {
     render() {
         return (
-            <TableRow>
-                 <TableCell><p>{this.props.birthday}</p></TableCell>
-                 <TableCell><p>{this.props.gender}</p></TableCell>
-                 <TableCell><p>{this.props.job}</p></TableCell>
-            </TableRow>
+                <TableRow>
+                    <TableCell><p>{this.props.birthday}</p></TableCell>
+                    <TableCell><p>{this.props.gender}</p></TableCell>
+                    <TableCell><p>{this.props.job}</p></TableCell>
+                </TableRow>
         );
     }
-}
+} */
 
 // Public Option
 export default Customer;
