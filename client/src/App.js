10f9b8f389e9defddd474593a601254a24fa1579
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-25 14:35:15 +0900

    AWS DB 연동 전까지 저장

--- client/src/App.js
+++ client/src/App.js
@@ -110,7 +110,13 @@ class App extends Component {
           <TableBody>
             {this.state.customers.map ? this.state.customers.map(c => {
                 return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
-              }) : ""}
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
