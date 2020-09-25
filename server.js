aa9e750962cec84b5e496bb080c56c7f2a86e3c2
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-25 16:24:22 +0900

    Mongo DB & Node 서버 연동, Schema(scarlet) 추가

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 const app = express();
-const port = process.env.PORT || 9983;
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+
+const port = process.env.PORT || 9983;
 
 app.get('/api/customers', (req, res) => {
     res.send([
@@ -27,4 +29,60 @@ app.get('/api/customers', (req, res) => {
     ]);
 });
 
+
+// 몽고 DB
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://root:tmdcks2416@localhost:27017/admin');
+// mongoose.connect('mongodb://username:password@host:port/database?options...');
+
  app.listen(port, () => console.log(`Listening on port ${port}`));
+
+/* ----- MongoDB -----*/
+
+// Schema 생성
+const user = mongoose.Schema({
+    no : 'number',
+    id : 'string',
+    nickname : 'string',
+    pw : 'string',
+    name : 'string',
+    birth : 'string',
+    gender : 'string',
+    email : 'string',
+    phone : 'number',
+    grade : 'number'
+});
+
+// 정의된 Schema를 객체처럼 사용할 수 있도록 model() 함수로 Compile 해줌.
+const User = mongoose.model('Schema', user);
+
+// user Schema(DB)에 새로운 collection(Table) 추가
+const newUser = new User({ no : '1', id : 'scarlet', nickname : 'scarlet', pw : 'tmdcks2416@', name : '이승찬', birth : '1996-02-08', gender : '남자', email : 'fu017@naver.com', phone : '01099834669', grade : '5' });
+
+// 세이브, 컬렉션 조회 방법
+/* 
+    // Data save
+    newUser.save(function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('User infomation has uploaded Succesfully.');
+        }
+    });
+
+    // user(Schema) Reference All Data Bringing...
+    User.find(function(error, users){
+        console.log('--- Read all ---');
+        if(error){
+            console.log(error);
+        }else{
+            console.log(users);
+        }
+    })
+ */
