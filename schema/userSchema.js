35f3a5d87f76ac4c6cdcf37671c60dda5da5de3b
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-28 14:34:53 +0900

    yarn 버그 수정 2차

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -35,37 +35,37 @@ const userSchema = new mongoose.Schema({
   직접 비밀번호가 일치하는지 확인하는 코드를 짤 필요없이 userSchema에 정의한 메소드로 재사용할 수 있다. 
   또 다큐먼트가 아닌 모델이나 쿼리에 직접 static method를 붙일 수도 있다. 
 */
-  userSchema.methods.comparePassword = function(pw ,cb)  {
+/*   userSchema.methods.comparePassword = function(pw ,cb)  {
     if (this.password === pw) {
       cb(null, true);
     } else {
       cb('password 불일치');
     }
-  };
+  }; */
 
   //나중에 user 다큐먼트를 받게 되면
-  user.comparePassword('비밀번호', function(err, result) {
+  /* userSchema.comparePassword('비밀번호', function(err, result) {
     if (err) {
       throw err;
     }
     console.log(result);
-  });
+  }); */
 
 //statics은 Users 모델에서 바로 쓸 수 있는 메소드이고, query는 한 번 쿼리 후에 이어서 쓰는 메소드이다.
-  userSchema.statics.findByBirth = function(birth) {
+/*   userSchema.statics.findByBirth = function(birth) {
     return this.find({ birth: { $gt: birth } });
   };
   userSchema.query.sortByName = function(order) {
     return this.sort({ nickname: order });
   };
-  User.findByName('이승찬').sortByName(-1);
+  User.findByName('이승찬').sortByName(-1); */
 
 
 /* 
   pre와 post Methods
   스키마에 붙여 사용하는데, 각각 특정 동작 이전, 이후에 어떤 행동을 취할 지를 정의할 수 있습니다. Hook를 건다고 생각하면 된다. 
 */
-  userSchema.pre('save', function(next) {
+/*   userSchema.pre('save', function(next) {
     if (!this.email) { // email 필드가 없으면 에러 표시 후 저장 취소
       throw '이메일이 없습니다';
     }
@@ -76,7 +76,7 @@ const userSchema = new mongoose.Schema({
   });
   userSchema.post('find', function(result) {
     console.log('저장 완료', result);
-  });
+  }); */
 
 
 /* 
--- server.js
+++ server.js
@@ -2,41 +2,18 @@ const express = require('express');
 const bodyParser  = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-
-// 잡 에러 제거
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-
-// 유저 스키마 DB 컬렉션 등록
-const userSchemaRG = require('./schema/userSchema');
+require('./schema/userSchema');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/api/customers', (req, res) => {
+// });
 
 const port = process.env.PORT || 9983;
-
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/128/128/any',
-            'name': '이승찬',
-            'birthday': '960208',
-            'gender': '남자',
-            'job': '취준생'
-            },
-            {
-            'id': 2,
-            'image': 'https://placeimg.com/128/129/any',
-            'name': '신영진',
-            'birthday': '961212',
-            'gender': '남자',
-            'job': '회사원'
-            }
-    ]);
-});
-
 
 // 몽고 DB
 const db = mongoose.connection;
@@ -48,3 +25,24 @@ db.once('open', function(){
 
 mongoose.connect('mongodb://scarlet:tmdcks2416@localhost:27017/admin', { useNewUrlParser: true, useUnifiedTopology: true });
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+// const User = database.collection('User');
+// // Default값으로 User컬렉션 다큐먼트 하나 추가
+// User.insertMany([{"nickname": "너구리", "password": "tmdcks2416", "name": "이승찬", "birth": "1996-02-08", 
+// "email": "fu017@naver.com", "phone": "01099834669", "grade": "5" }], function(err, result) {
+//     if (err) {  // 에러 발생 시 콜백 함수를 호출하면서 에러 객체 전달
+//      callback(err, null);
+//      return;
+//     }
+
+//     // 에러 아닌 경우, 콜백 함수를 호출하면서 결과 객체 전달
+//     if (result.insertedCount > 0) {
+//         console.log("사용자 레코드 추가됨 : " + result.insertedCount);
+//     } else {
+//         console.log("추가된 레코드가 없음.");
+//     }
+       
+//     callback(null, result);
+// });
