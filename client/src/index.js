e88dc0449bfcdc9c3bb03a0b808657ffcefecbcb
fu013 <66549891+fu013@users.noreply.github.com>
2020-09-29 15:21:19 +0900

    노드서버 FRONT 작업 스케치 완료

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,16 @@
-// 리엑트안의 컴포넌트를 { Component } 라는 이름으로 쓰겠다. 비구조화 할당
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import {HashRouter} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-/* import Header from "./Header";
+import Header from "./Header";
 import Body from "./Body";
-import Footer from "./Footer"; */
-import './index.css';
-import App from './App';
+import Footer from "./Footer";
 
 ReactDOM.render(
   <HashRouter>
-{/*     <Header/>
+    <Header/>
     <Body/>
-    <Footer/> */}
+    <Footer/>
   </HashRouter>,
-  document.getElementById('root')
+  document.querySelector("#container")
 );
-
-// 4강 17분까지 봄 <햄릿슈>
